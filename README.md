3b2262af3802fb8e898b1179b36fb59403d20654
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-10-04 08:28:17 +0200

    GItHub user and owner+repo combined

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Container Action to generate a repository dispatch event
+
+Dispatch a repository event
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,24 @@
 #!/bin/sh -l
 
-OWNER=$1
-REPO=$2
-TOKEN=$3
+REPO=$1
+TOKEN=$2
+USER=$3
 ACTION=$4
 
-if [ -z "${OWNER}" ]; then
-	echo "Missing environment variable OWNER"
-	exit 1
-fi
 if [ -z "${REPO}" ]; then
-	echo "Missing environment variable REPO"
+	echo "You must provide a repository to dispatch event to (:owner/:repo)"
 	exit 1
 fi
 if [ -z "${TOKEN}" ]; then
-	echo "Missing environment variable TOKEN"
+	echo "You must provide a GitHub authentication token"
+	exit 1
+fi
+if [ -z "${USER}" ]; then
+	echo "You must provide the GitHub user to which the authentication token belong"
 	exit 1
 fi
 
-curl -X POST https://api.github.com/repos/${OWNER}/${REPO}/dispatches \
+curl -X POST https://${USER}@api.github.com/repos/${REPO}/dispatches \
 -H 'Accept: application/vnd.github.everest-preview+json' \
 -H 'Authorization: token ${TOKEN}' \
 --data '{"event_type": "${ACTION}"}'
